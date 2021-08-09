0afd96f21ef03f8d8042a71176af672120daeb38
m.piran <piran_mahsa@yahoo.com>
2021-08-09 11:41:02 +0430

    Filter todos

---
+++ src/Components/App.js
@@ -0,0 +1,79 @@
+import React, {Component} from 'react';
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+//import components
+import Header from "./Header";
+import FormAddTodo from "./FormAddTodo";
+import Todo from "./Todo";
+
+class App extends Component {
+
+    //initialize state
+    state = {
+        todos: [],
+        statusDone : false
+    }
+
+    addTodo(text) {
+        this.setState(prevState => {
+            return {
+                todos: [...prevState.todos,
+                    {key: Date.now(), done: false, text}]
+            }
+        })
+    }
+
+
+    render() {
+        let {todos , statusDone} = this.state
+        let filterTodos= todos.filter(item=>item.done===statusDone)
+        // console.log(filterTodos)
+
+        return (
+            <div className="App">
+                <Header/>
+                <main>
+                    <section className="jumbotron">
+                        <div className="container d-flex flex-column align-items-center">
+                            <h1 className="jumbotron-heading">Welcome!</h1>
+                            <p className="lead text-muted">To get started, add some items to your list:</p>
+                            <FormAddTodo add={this.addTodo.bind(this)}/>
+                        </div>
+                    </section>
+                    <div className="todosList">
+                        <div className="container">
+                            <div className="d-flex flex-column align-items-center ">
+                                <nav className="col-6 mb-3">
+                                    <div className="nav nav-tabs" id="nav-tab" role="tablist">
+                                        <a className={`nav-item nav-link font-weight-bold ${statusDone ? '' : 'active' }`}
+                                           onClick={()=>this.setState({statusDone: false})}
+                                           d="nav-home-tab">undone <span className="badge badge-secondary">
+                                            {todos.filter(item=> item.done===false).length}
+                                        </span></a>
+                                        <a className={`nav-item nav-link font-weight-bold ${statusDone ? 'active' : '' }`}
+                                           onClick={()=>this.setState({statusDone: true})}
+                                           id="nav-profile-tab">done <span className="badge badge-success">
+                                            {todos.filter(item=> item.done===true).length}
+                                        </span></a>
+                                    </div>
+                                </nav>
+                                    {
+                                        todos.length === 0
+                                            ? <p>There is no todos</p>
+                                            : filterTodos.map(item => <Todo key={item.key} text={item.text}/>)
+                                    }
+
+
+                            </div>
+
+                        </div>
+                    </div>
+                </main>
+            </div>
+        )
+    }
+}
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+ReactDOM.render(<App />,
   document.getElementById('root')
 );
 
